72820b3c740cdd687f1c9e379a3ac0ddc16dd31f
bcourter <git-bcourter.gmail.com>
2014-03-15 22:31:34 -0400

    new meshes, a bit large

--- js/penrose.js
+++ js/penrose.js
@@ -1,8 +1,6 @@
 $(function() {
 
 var renderer, camera, settings, bodyGeometry, lightGeometry, dudesGeometry, triangle, scene, globe;
-var modelLoaded = false;
-var dudesLoaded = false;
 var lightHue = 0.6;
 var triangleColor = 0x333333;
 var ambientColor = 0x222222;
@@ -21,7 +19,7 @@ function initUI() {
 		$("#isRotating").button();
 		$("#isAnimating").button();
 		$("#showPeople").button().click(function() {
-			if (!dudesLoaded) {
+			if (dudesGeometry === undefined) {
 				loadDudes();
 			} else {
 				dudesGeometry.traverse( function ( child ) {
@@ -46,10 +44,10 @@ function initRenderer() {
 	var panorama = getGetValue("panorama");
 	if (panorama != null) {
 		triangleColor = 0xffffff;
-		ambientColor = 0xaaaaaa;
+		ambientColor = 0x777777;
 	}
 
-	camera = new THREE.PerspectiveCamera(5, window.innerWidth / window.innerHeight, 1, 1000);
+	camera = new THREE.PerspectiveCamera(4, window.innerWidth / window.innerHeight, 1, 1000);
 	camera.position.z = 90;
 	camera.position.x = 50;
 
@@ -100,48 +98,81 @@ function initRenderer() {
 	scene.add(globe);
 
 	var loader = new THREE.OBJLoader();
-	var bodyLoaded = false;
-	var lightsLoaded = false;
 
 	loader.load( "resources/obj/penrose-body.obj", function ( event ) {
 		bodyGeometry = event.clone();			
 		triangle.add(bodyGeometry);
-		if (lightsLoaded)
-			finishModelLoad();
-		else
-			bodyLoaded = true;
+
+		bodyGeometry.traverse( function ( child ) {
+			if ( child instanceof THREE.Mesh ) {			
+				child.material = new THREE.MeshPhongMaterial( { 
+					color: triangleColor, 
+					emissive: 0x333333, 
+					shading: THREE.SmoothShading,
+					specular: triangleColor,
+					shininess: 20
+				} );
+
+				child.castShadow = true;
+	    		child.receiveShadow = true;
+			}
+		});
+
+		bodyGeometry.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2));
 	});
 	
 	var lightLoader = new THREE.OBJLoader();
 	lightLoader.load( "resources/obj/penrose-lights.obj", function ( event ) {
 		lightGeometry = event.clone();
 		triangle.add(lightGeometry);
-		if (bodyLoaded)
-			finishModelLoad();
-		else
-			lightsLoaded = true;
+
+		lightGeometry.traverse( function ( child ) {
+			if ( child instanceof THREE.Mesh ) 		
+				child.material = new THREE.MeshBasicMaterial( { color: 0xffffff, shading: THREE.FlatShading } );
+		});
+
+		lightGeometry.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2));
 	});
 
-	var scale = 0.03;
+	
+	var scale = 1;
 	triangle.scale.set(scale, scale, scale);
 	triangle.position.y = floorHeight;
 	
+	globe.add(triangle);
+
 	// panorama
 	if (panorama != null) {
 	    panomap = THREE.ImageUtils.loadTexture("resources/panoramas/" + panorama);
 
-	    var sphere = new THREE.SphereGeometry( 360, 64, 48 );
+	    var radius = 7;
+	    var sphere = new THREE.SphereGeometry( radius, 64, 48 );
 	    sphere.applyMatrix( new THREE.Matrix4().makeScale( -1, 1, 1 ) );
 	    sphere.applyMatrix( new THREE.Matrix4().makeRotationY(1.75));
 
+	    var sphereFloor = 0.1
+	    var sphereCap = radius * 3 / 4;
 	    for (var i = 0; i < sphere.vertices.length; i++) {
 	    	var vertex = sphere.vertices[i];
-	    	vertex.y = Math.max(vertex.y - 2, 0);
+	    	vertex.y = Math.max(vertex.y + sphereFloor, 0);
+
+	    	if (vertex.y > sphereFloor) {
+	    		r = Math.sqrt(vertex.x * vertex.x + vertex.z * vertex.z);
+	    		if (r > 0.001) {
+		    		var factor = 0;
+		    		if (vertex.y > sphereCap)
+		    			factor = (vertex.y - sphereCap) / (radius - sphereCap);
+
+
+		    		vertex.x *= (1-factor) * radius / r + factor;
+		    		vertex.z *= (1-factor) * radius / r + factor;
+		    	}
+	    	}
 	    }
 
 	    var sphereMaterial = new THREE.MeshLambertMaterial( {
-	    	color: 0x888888,
-	    	emissive: 0x111111,
+	    	color: 0xdddddd,
+	    	emissive: 0xaaaaaa,
 	        map: panomap,
 	    } );
 
@@ -149,26 +180,26 @@ function initRenderer() {
 	    skybox.receiveShadow = true;
 	    triangle.add(skybox);
 
-	    light = new THREE.SpotLight( 0xffffff, 1, 0, Math.PI / 2, 1 );
-		light.position.set( -800, 1000, 1300 );
-		light.target.position.set( 0, 0, 0 );
+	    light = new THREE.SpotLight( 0x333333, 1, 0, Math.PI / 2, 1 );
+		light.position.set( -24, 30, 40 );
+		light.target.position.set( 0, -2, 0 );
 
-	//	light.shadowCameraVisible = true;
+	//light.shadowCameraVisible = true;
 
 	    light.castShadow = true;
-	    light.onlyShadow = true;
+	//    light.onlyShadow = true;
 
-	    var size = 20;
+	    var size = 10;
 	    light.shadowCameraLeft = -size;
 	    light.shadowCameraTop = -size;
 	    light.shadowCameraRight = size;
 	    light.shadowCameraBottom = size;
 	    light.shadowCameraNear = 30;
 	    light.shadowCameraFar = 80;
-	    light.shadowCameraFov = 5.2;
-	    light.shadowBias = 0.001
+	    light.shadowCameraFov = 5.5
+	    light.shadowBias = 0.01
 	    light.shadowMapWidth = light.shadowMapHeight = 128;
-	    light.shadowDarkness = 0.4;  
+	    light.shadowDarkness = 0.3;  
 
 
 		triangle.add(light);
@@ -184,7 +215,11 @@ function initRenderer() {
 	var directionalLight = new THREE.DirectionalLight(0x404040);
 	directionalLight.position.set(1, 1, 1).normalize();
 //	scene.add(directionalLight);
-}
+
+ 	loadOtherObj("penrose-base.obj", 0x000000);
+ 	loadOtherObj("penrose-flanges.obj", 0xffffff);
+ 	loadOtherObj("penrose-hardware.obj", 0x111111);
+ }
 
 function getGetValue(key){
 	var location = window.location.search;
@@ -195,43 +230,50 @@ function getGetValue(key){
 	return res[1];
 }	
 
-function finishModelLoad() {
-	bodyGeometry.traverse( function ( child ) {
-		if ( child instanceof THREE.Mesh ) {			
-			child.material = new THREE.MeshPhongMaterial( { 
-				color: triangleColor, 
-				emissive: 0x111111, 
-				shading: THREE.SmoothShading,
-			} );
-
-			child.castShadow = true;
-    		child.receiveShadow = true;
-		}
-	});
-
-	lightGeometry.traverse( function ( child ) {
-		if ( child instanceof THREE.Mesh ) {			
-			child.material = new THREE.MeshBasicMaterial( { color: 0xffffff, shading: THREE.FlatShading } );
-		}
-	});
-
-	globe.add(triangle);
-	modelLoaded = true;
-}
-
 function loadDudes() {
 	var dudeLoader = new THREE.OBJLoader();
-	dudeLoader.load( "resources/obj/dudes0.obj", function ( event ) {
-		dudesGeometry = event.content.clone();
-		dudesGeometry.applyMatrix(new THREE.Matrix4().identity().rotateX(-Math.PI / 2));
+	dudeLoader.load( "resources/obj/dude.obj", function ( event ) {
+		dudesGeometry = event.clone();
+		dudesGeometry.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2));
 		dudesGeometry.traverse( function ( child ) {
 			if ( child instanceof THREE.Mesh ) {			
-				child.material = new THREE.MeshPhongMaterial( { color: 0xeebb44, emissive: 0x664422, shading: THREE.SmoothShading } );
+				child.material = new THREE.MeshPhongMaterial( { 
+					color: 0x0, 
+					emissive: 0x0, 
+					shading: THREE.SmoothShading,
+					specular: 0x442211,
+					shininess: 15
+				 } );
 			}
+
+			child.castShadow = true;
 		});
 		
 		triangle.add(dudesGeometry);
-		dudesLoaded = true;
+	});
+}
+
+function loadOtherObj(name, color) {
+	var loader = new THREE.OBJLoader();
+	loader.load( "resources/obj/" + name, function ( event ) {
+		var obj = event.clone();
+		obj.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2));
+		obj.traverse( function ( child ) {
+			if ( child instanceof THREE.Mesh ) {			
+				child.material = new THREE.MeshPhongMaterial( { 
+					color: color, 
+					emissive: new THREE.Color(color).lerp(new THREE.Color(0x0), 0.8).getHex(),	
+					shading: THREE.SmoothShading,
+					specular: color,
+					shininess: 5
+				 } );
+			}
+
+			if (name == "penrose-flange.obj")
+				child.castShadow = true;
+		});
+		
+		triangle.add(obj);
 	});
 }
 
@@ -268,10 +310,6 @@ function animate() {
 }
 
 function updateModel() {
-	if (!modelLoaded) {
-		return;
-	}
-
 	var rotationFactor = 0.001
 	if (settings.isRotatingCheckbox.checked) {
 		triangle.rotation.y += rotationFactor;
@@ -283,7 +321,7 @@ function updateModel() {
 }
 
 function render() {
-	if (settings.isAnimatingCheckbox.checked && modelLoaded) {
+	if (settings.isAnimatingCheckbox.checked && lightGeometry !== undefined) {
 		lightGeometry.traverse( function ( child ) {
 			if ( child instanceof THREE.Mesh ) {
 				child.material.color.setHSL(lightHue, 1.0, 0.6);
